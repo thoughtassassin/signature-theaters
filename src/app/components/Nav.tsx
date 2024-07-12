8545dea831fc3dfe31746e4317da5a3982eedc41
David <david@onerootelement.com>
2024-07-12 04:36:07 -0600

    Add scroll animated navigation

---
+++ src/app/components/Nav.tsx
@@ -0,0 +1,22 @@
+import { motion } from "framer-motion";
+
+interface NavProps {
+  children: React.ReactNode;
+  isFixed: boolean;
+}
+
+const Nav = ({ children, isFixed }: NavProps) => {
+  return (
+    <motion.nav
+      className={
+        isFixed
+          ? "fixed flex justify-between items-center pt-4 pb-8 px-28 w-full bg-gradient-to-b from-black from-60% to-transparent"
+          : "flex justify-between items-center py-16 px-28 w-full bg-gradient-to-b from-black to-transparent"
+      }
+    >
+      {children}
+    </motion.nav>
+  );
+};
+
+export default Nav;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,52 @@
+'use client';
+
+import { useRef, useEffect, useState } from "react";
 import Image from "next/image";
-import { Play } from "next/font/google";
+import { Play, Exo_2 } from "next/font/google";
 import Head from "next/head";
+import { useInView } from "framer-motion";
+import Nav from "@/app/components/Nav";
 
 const play = Play({ weight:['400'], subsets: ["latin"] });
+const exo2 = Exo_2({ weight: ["400"], subsets: ["latin"] });
 
 export default function Home() {
+  const heroRef = useRef<null | HTMLDivElement>(null);
+  const [isNavFixed, setIsNavFixed] = useState(false); 
+  const isInView = useInView(heroRef, { amount: 0.15 });
+
+  useEffect(() => {
+    if (isInView) {
+      setIsNavFixed(false);
+    } else {
+      setIsNavFixed(true);
+    }
+  }, [isInView]);
+  
   return (
     <>
       <Head>
         <link rel="preload" href="/signature-theaters-02239.jpg" as="image" />
       </Head>
       <main>
-        <div className="h-screen bg-cover bg-[url('/signature-theaters-02239.jpg')] bg-fixed">
-          <nav className="flex justfiy-between items-center py-16 px-28 w-full bg-gradient-to-b from-black to-transparent">
+        <div
+          ref={heroRef}
+          className="h-screen bg-cover bg-[url('/signature-theaters-02239.jpg')] bg-fixed"
+        >
+          <Nav isFixed={isNavFixed}>
             <Image
               src="/signature-theaters-logo.svg"
               alt="logo"
-              width={250}
+              width={isNavFixed ? 175 : 250}
               height={100}
             />
-            <ul className="flex justify-end gap-12 grow items-center text-xl">
+            <ul
+              className={
+                isNavFixed
+                  ? `${exo2.className} flex justify-end gap-12 grow items-center text-l tracking-wider`
+                  : `${exo2.className} flex justify-end gap-12 grow items-center text-xl tracking-wider`
+              }
+            >
               <li>
                 <a href="/services">Services</a>
               </li>
@@ -33,13 +60,20 @@ export default function Home() {
                 <a href="/contact">Contact</a>
               </li>
             </ul>
-          </nav>
+          </Nav>
+        </div>
+        <div className="flex flex-col items-center justify-start h-screen p-16 bg-gradient-to-b from-black to-[#111]">
+          <h1
+            className={`${play.className} text-6xl text-center text-[#CE0E2D] tracking-widest uppercase mt-8`}
+          >
+            Signature Services
+          </h1>
         </div>
         <div className="flex flex-col items-center justify-start h-screen p-16 bg-gradient-to-b from-black to-[#111]">
           <h1
             className={`${play.className} text-6xl text-center text-[#FFC629] tracking-widest uppercase mt-8`}
           >
-            Signature Services
+            Signature Products
           </h1>
         </div>
       </main>
