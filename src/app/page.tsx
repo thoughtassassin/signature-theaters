cc89b8e3394acf655d745cde2af1847f2c973066
David <david@onerootelement.com>
2024-07-19 14:18:33 -0600

    Add background hero images Create image list Create Hero component Move useInView to Hero component Create interval in Hero component to rotate images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,27 @@
-'use client';
+"use client";
 
-import { useRef, useEffect, useState } from "react";
+import { useState } from "react";
 import Image from "next/image";
 import { Play, Exo_2 } from "next/font/google";
 import Head from "next/head";
-import { useInView } from "framer-motion";
 import Nav from "@/app/components/Nav";
+import Hero from "@/app/components/Hero";
 
-const play = Play({ weight:['400'], subsets: ["latin"] });
+import { homepage_hero_photos } from "@/app/utils/lists";
+
+const play = Play({ weight: ["400"], subsets: ["latin"] });
 const exo2 = Exo_2({ weight: ["400"], subsets: ["latin"] });
 
 export default function Home() {
-  const heroRef = useRef<null | HTMLDivElement>(null);
-  const [isNavFixed, setIsNavFixed] = useState(false); 
-  const isInView = useInView(heroRef, { amount: 0.15 });
+  const [isNavFixed, setIsNavFixed] = useState(false);
 
-  useEffect(() => {
-    if (isInView) {
-      setIsNavFixed(false);
-    } else {
-      setIsNavFixed(true);
-    }
-  }, [isInView]);
-  
   return (
     <>
       <Head>
         <link rel="preload" href="/signature-theaters-02239.jpg" as="image" />
       </Head>
       <main>
-        <div
-          ref={heroRef}
-          className="h-screen bg-cover bg-[url('/signature-theaters-02239.jpg')] bg-fixed"
-        >
+        <Hero list={homepage_hero_photos} setIsNavFixed={setIsNavFixed}>
           <Nav isFixed={isNavFixed}>
             <Image
               src="/signature-theaters-logo.svg"
@@ -61,7 +50,7 @@ export default function Home() {
               </li>
             </ul>
           </Nav>
-        </div>
+        </Hero>
         <div className="flex flex-col items-center justify-start h-screen p-16 bg-gradient-to-b from-black to-[#111]">
           <h1
             className={`${play.className} text-6xl text-center text-[#CE0E2D] tracking-widest uppercase mt-8`}
---
+++ src/app/utils/lists.tsx
@@ -0,0 +1,58 @@
+export interface HeroPhoto {
+  id: number;
+  src: string;
+  alt: string;
+};
+
+export const homepage_hero_photos : HeroPhoto[] = [
+  {
+    id: 1,
+    src: "hord_cuthbert_1.jpg",
+    alt: "Hord Cuthbert living room television",
+  },
+  {
+    id: 2,
+    src: "hord_cuthbert_2.jpg",
+    alt: "Hord Cuthbert television over pool bar",
+  },
+  {
+    id: 3,
+    src: "saulsbury_hangar_1.jpg",
+    alt: "Saulsbury Hangar interior lighting in bedroom",
+  },
+  {
+    id: 4,
+    src: "saulsbury_hangar_2.jpg",
+    alt: "Saulsbury Hangar main floor with television in bar",
+  },
+  {
+    id: 5,
+    src: "saulsbury_san_subia_1.jpg",
+    alt: "Saulsbury San Subia exterior television with pool in foreground",
+  },
+  {
+    id: 6,
+    src: "saulsbury_san_subia_2.jpg",
+    alt: "Saulsbury San Subia television in animal themed bedroom with animal trophy heads",
+  },
+  {
+    id: 7,
+    src: "smith_kahala_1.jpg",
+    alt: "Smith Kahala television in home theater with modern art",
+  },
+  {
+    id: 8,
+    src: "smith_kahala_2.jpg",
+    alt: "Smith Kahala stereo with modern art mounted on wall in background",
+  },
+  {
+    id: 9,
+    src: "smith_kahala_3.jpg",
+    alt: "Smith Kahala exterior with television mounted above outdoor fireplace",
+  },
+  {
+    id: 10,
+    src: "steven_winfield_1.jpg",
+    alt: "Steven Winfield living room with sliding panel television above fireplace",
+  },
+];
